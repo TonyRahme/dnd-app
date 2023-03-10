ff21d06e07f5ec5ed6bf75f3d3d0d25e0bb50fbb
TonyRahme <tonyrahme96@gmail.com>
2023-03-10 03:22:44 -0500

    refactor+feat(entity-generator-service)
    -added function overloads for genDimension
    -refactored functions genExit, genPassageWay, genDoor
    -added genExitPointsByRoomId, getRelativeDirection
    -added util functions to get cardinal direction name or vector

--- src/components/random-dungeon-gen/services/entity-generator.service.ts
+++ src/components/random-dungeon-gen/services/entity-generator.service.ts
@@ -1,5 +1,5 @@
 import { RegexDungeonRules } from "../dungeon-graph.config";
-import { Chamber, Dimension, CardinalDirectionVector2, Door, DoorType, ExitDTO, ExitType, Passage, PassageWay, RoomEntityModelRequest, RoomShapeType, Vector2, CardinalDirectionName } from "../random-dungeon-gen.model";
+import { Chamber, Dimension, CardinalDirectionVector2, Door, DoorType, ExitDTO, ExitType, Passage, PassageWay, RoomEntityModelRequest, RoomShapeType, Vector2, CardinalDirectionName, Vector3 } from "../random-dungeon-gen.model";
 
 export class EntityGenerator  {
 
@@ -22,20 +22,42 @@ export class EntityGenerator  {
                 return RoomShapeType.None;
         }
     }
-                        
-    static genDimension = (length=0, width=0, x=0, y=0,z=0, dir = CardinalDirectionName.East): Dimension => {
+    
+    
+    public static genDimension(dimension2D: Vector2, position3D: Vector3, dir?: CardinalDirectionName): Dimension;
+    public static genDimension(length?:number, width?:number, x?:number, y?:number, z?:number, dir?: CardinalDirectionName):Dimension;
+    public static genDimension(lengthOrDimension?:any, widthOrPosition?:any, xOrDir?:any, y=0,z=0, dir = CardinalDirectionName.East): Dimension {
+        //x is length and y is width
+        let dimension: Vector2 =  typeof lengthOrDimension == 'number' || lengthOrDimension === undefined ?
+        {
+            x: lengthOrDimension ? lengthOrDimension:0,
+            y: widthOrPosition ? widthOrPosition:0,
+        }
+        :lengthOrDimension; 
+        
+        let position: Vector3 = typeof widthOrPosition == 'number' || widthOrPosition === undefined ?
+        {
+            x:typeof xOrDir == 'number'? xOrDir:0,
+            y:y,
+            z:z
+        }
+        : widthOrPosition;
+
+        if(typeof xOrDir == 'string'){
+            dir = xOrDir as CardinalDirectionName;
+        }
         
         let isHorizontal = CardinalDirectionVector2[dir].x !== 0;
-        let centerX = isHorizontal ? x + length/2 : x + width/2;
-        let centerY = isHorizontal ? y + width/2 : y + length/2;
+        let centerX = isHorizontal ? position.x + dimension.x/2 : position.x + dimension.y/2;
+        let centerY = isHorizontal ? position.y + dimension.y/2 : position.y + dimension.x/2;
         return {
-            length: length,
-            width: !width ? length : width,
+            length: dimension.x,
+            width: !dimension.y ? dimension.x : dimension.y,
             height: 10,
             center: {x: centerX, y: centerY},
-            x: x,
-            y: y,
-            z: z,
+            x: position.x,
+            y: position.y,
+            z: position.z,
             direction: dir,
         }
     }
@@ -83,19 +105,87 @@ export class EntityGenerator  {
         }
     } 
 
-    static genExit = (exitId: string, exitType: ExitType, isSecret: boolean = false): ExitDTO => {
+    static genExit = (
+        exitId: string,
+        exitType: ExitType,
+        position: Vector2,
+        direction = CardinalDirectionName.East,
+        isSecret = false,
+        isTrap = false,
+        isLocked = false
+        ): ExitDTO => {
         
         const exit: ExitDTO = {
-            exitType: exitType,
             exitId: exitId,
+            exitType: exitType,
             isSecret: isSecret,
+            position: position,
+            direction: direction,
+            isTrap: isTrap,
+            isLocked: isLocked,
         }
         return exit;
     }
 
-    static genPassageWay = (newId: string, newRoomIds: string[]): PassageWay => {
+    static genExitPointsByRoomId = (roomDimension: Dimension, exitCount: number): Vector2[] => {
+        
+        let directionVector = CardinalDirectionVector2[roomDimension.direction];
+        
+        let radianDelta = 2*Math.PI / exitCount;
+        let startRadian = Math.acos(-directionVector.x) + Math.asin(-directionVector.y);
+        let deltaRadian = startRadian;
+
+        let absDimension = EntityGenerator.checkRotationDimensions(roomDimension.direction, roomDimension);
+        let exitPositions: Vector2[] = [];
+        
+        for(let i = 0;i<exitCount;i++){
+            deltaRadian += radianDelta;
+            exitPositions.push({
+                x: roomDimension.x + absDimension.x*Math.cos(deltaRadian)/2 + roomDimension.center.x,
+                y: roomDimension.y + absDimension.y*Math.sin(deltaRadian)/2 + roomDimension.center.y,
+            });
+        }
+        return exitPositions;
+    }
+
+    static getRelativeDirection = (doorPosition: Vector2, roomDimension: Dimension): CardinalDirectionName => {
+        const roomStartPosition = new Vector2(roomDimension.x, roomDimension.y);
+        const roomEndPosition = new Vector2(roomStartPosition.x + roomDimension.length, roomStartPosition.y + roomDimension.width);
+    
+        const cardinalX = doorPosition.x === roomStartPosition.x ? -1 : doorPosition.x === roomEndPosition.x ? 1 : 0;
+        const cardinalY = doorPosition.y === roomStartPosition.y ? 1 : doorPosition.y === roomEndPosition.y ? -1 : 0;
+        
+        const cardinal2D = new Vector2(cardinalX, cardinalY);
+        
+        const cardinalName = this.getCardinalDirectionNameByVector(cardinal2D);
+        
+        return cardinalName;
+    }
+
+    static getCardinalDirectionNameByVector = (vector2: Vector2): CardinalDirectionName => {
+        Object.entries(CardinalDirectionVector2).forEach(([cardinalName, cardinalVector]) => {
+            if(vector2.x === cardinalVector.x && vector2.y === cardinalVector.y){
+                return cardinalName;
+            }
+        });
+        //defaults to East;
+        return CardinalDirectionName.East;
+    }
+
+    static getCardinalDirectionVectorByName = (name: CardinalDirectionName): Vector2  => {
+        Object.entries(CardinalDirectionVector2).forEach(([cardinalName, cardinalVector]) => {
+            if(name === cardinalName){
+                return cardinalVector;
+            }
+        });
+        //defaults to East;
+        return CardinalDirectionVector2[CardinalDirectionName.East];        
+    }
+
+    static genPassageWay = (newId: string, newRoomIds: string[], position: Vector2): PassageWay => {
+        //TODO: worry about direction of the passage way facing
         let newPassageWay: PassageWay = {
-            dimension: this.genDimension(0,5),
+            dimension: this.genDimension(0,5, position.x, position.y),
             exitType: ExitType.Passage,
             id: newId,
             roomIds: newRoomIds,
@@ -104,9 +194,13 @@ export class EntityGenerator  {
         return newPassageWay;
     }
 
-    static genDoor = (newId: string, newDoorType: DoorType, newRoomIds: string[], isLocked = false, isTrap = false, isSecret = false): Door => {
+    static genDoor = (newId: string, newDoorType: DoorType, newRoomIds: string[], exit: ExitDTO): Door => {
+        //TODO: Worry about direction of door facing
+        const isLocked = exit.isLocked;
+        const isTrap = exit.isTrap;
+        const isSecret = exit.isSecret;
         let newDoor: Door = {
-            dimension: this.genDimension(0,5),
+            dimension: this.genDimension(0,5, exit.position.x, exit.position.y),
             exitType: ExitType.Door,
             id: newId,
             doorType: newDoorType,
